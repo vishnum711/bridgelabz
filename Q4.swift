207181040f208dd8bd0ce117f87bc1a726b13547
vishnum711 <54273320+vishnum711@users.noreply.github.com>
2019-08-20 19:09:40 +0530

    question 4

---
+++ Q4.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  power_of_N
+//
+//  Created by BridgeLabz on 20/08/19.
+//  Copyright © 2019 Bridgelabz. All rights reserved.
+//
+
+import Foundation
+print("enter the power less than 31")
+var num = readLine()
+var powernum = Int(num!)
+var i = 1
+var n = 1
+while(i <= powernum!){
+    n = n*2
+    i=i+1
+}
+if(n%4 == 0){
+    print("the year is leap")
+}
+else{
+    print("the year is not leap")
+
+}
