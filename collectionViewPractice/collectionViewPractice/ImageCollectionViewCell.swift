691915f70305627931e1e1f5ed1ac793ae434c3f
vishnum711 <54273320+vishnum711@users.noreply.github.com>
2019-10-07 12:32:44 +0530

    cell move

---
+++ collectionViewPractice/collectionViewPractice/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  collectionViewPractice
+//
+//  Created by admin on 9/30/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myLabelView: UILabel!
+}
