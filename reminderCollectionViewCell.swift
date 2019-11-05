b9fe303d097687a724d88bfd3e22ac4a011b28d6
vishnum711 <54273320+vishnum711@users.noreply.github.com>
2019-11-05 15:09:13 +0530

    Add files via upload

---
+++ reminderCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  reminderCollectionViewCell.swift
+//  loginApp
+//
+//  Created by admin on 10/15/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class reminderCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var remTextField: UITextView!
+    
+}
